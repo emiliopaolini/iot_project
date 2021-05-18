376855cf5f5903aee9f972603adeadec20d39db5
emiliopaolini <paolini.emilio5@gmail.com>
2021-05-18 15:02:18 +0200

    First server

--- project/src/main/java/project/ResourceRegistration.java
+++ project/src/main/java/project/ResourceRegistration.java
@@ -18,12 +18,22 @@ public class ResourceRegistration extends CoapResource {
 	 */
 	public void handlePOST(CoapExchange exchange) {
 		System.out.println("Request of a new registration received...");
-		//exchange.respond(ResponseCode.CREATED);
+		// parsing data from the sensor
+		JSONObject contentJson = new JSONObject(new String(exchange.getRequestPayload()));
+		String nodeIP = exchange.getSourceAddress().getHostAddress();
+		System.out.println("Node ["+nodeIP+"] registration...");
+		String nodeType = (String) contentJson.get("Type");
+		String nodeResource = (String) contentJson.get("Resource");
+
 		Response response = new Response(ResponseCode.CONTENT);
 		if(exchange.getRequestOptions().getAccept() == MediaTypeRegistry.APPLICATION_JSON) {
 			response.setPayload("registered");
 			response.getOptions().setContentFormat(MediaTypeRegistry.APPLICATION_JSON);
 		}
 		exchange.respond(response);
+
+		// here we have to create the coap client for the resource of the sensor/actuator
+
+		// a coap client for each registered node to observe the resource
 
 }}
--- sensor_oxygen/oxygen_sensor.c
+++ sensor_oxygen/oxygen_sensor.c
@@ -21,6 +21,7 @@
 #define SERVER_EP "coap://[fd00::1]:5683"
 
 #define TIMER_PERIOD 60
+#define REGISTRATION_PERIOD 30
 
 
 
@@ -35,12 +36,8 @@ bool registered = false;
 
 
 
-    
-
-
 void wait_for_ack(coap_message_t *response) {
-printf("response received\n");
-/*
+    printf("response received\n");
     if(response == NULL) { 
         LOG_DBG("No response to registration..."); 
         return;
@@ -48,22 +45,8 @@ printf("response received\n");
     
     if(strcmp((const char *)response->payload, "registered") == 0)
     	registered =  true;
-*/}
+}
 
-
-
-//void server_registration(){
-   // char *service_url = "/registration";
-    // Prepare the message
-    //coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
-   // coap_set_header_uri_path(request, service_url);
-    // Set the payload 
-    //const char msg[] = "{\"Type\":\"sensor\", \"Resource\":\"oxygen\"}";
-    //coap_set_payload(request, (uint8_t *)msg, sizeof(msg) - 1);
-    // Issue the request in a blocking manner
-    // The client will wait for the server to reply (or the transmission to timeout)
-    //COAP_BLOCKING_REQUEST(&server_ep, request, wait_for_ack);
-//}
 
 
 PROCESS(oxygen_sensor, "Oxygen sensor");
@@ -76,8 +59,9 @@ PROCESS_THREAD(oxygen_sensor, ev, data) {
 	//static coap_endpoint_t actuator_ep;
 	static coap_message_t request[1];
 	static coap_endpoint_t server_ep;
-//initialize the timer
+    //initialize the timer
 	static struct etimer timer; //timer to randomly change the oxygen
+    static struct etimer registration_timer; //timer to randomly change the oxygen
     PROCESS_BEGIN();
     
     LOG_INFO("Oxygen sensor: starting.... \n");
@@ -85,41 +69,57 @@ PROCESS_THREAD(oxygen_sensor, ev, data) {
     coap_activate_resource(&oxygen_measuring_device, "oxygen");
     
 	coap_endpoint_parse(SERVER_EP, strlen(SERVER_EP), &server_ep); //initialize server endpoint
-    // SERVER REGISTRATION 
-    //do{
-        //server_registration();
-	    char *service_url = "/registration";
-	    // Prepare the message
-	    coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
-	    coap_set_header_uri_path(request, service_url);
-	    // Set the payload 
-	    const char msg[] = "{\"Type\":\"sensor\", \"Resource\":\"oxygen\"}";
-	    coap_set_payload(request, (uint8_t *)msg, sizeof(msg) - 1);
-	    // Issue the request in a blocking manner
-	    // The client will wait for the server to reply (or the transmission to timeout)
-printf("before blocking...\n");
-	    COAP_BLOCKING_REQUEST(&server_ep, request, wait_for_ack);
-printf("after blocking...\n");
-    //}while(!registered);
-    // END SERVER REGISTRATION
+    
 
 	
 	
 	
     etimer_set(&timer, CLOCK_SECOND*TIMER_PERIOD);
+    etimer_set(&registration_timer, CLOCK_SECOND*REGISTRATION_PERIOD);
 	
 	printf("Timer inizialized\n");
 
     while (1) {
 
-        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
-       	printf("trigerring device...\n");
-       // if (etimer_expired(&timer)){
-            oxygen_measuring_device.trigger(); //this will call the res_event_handler of the resource that will randomly change the oxygen level
-		   //reset random timer
-		    etimer_reset(&timer);
-		//}
-	printf("device is triggered...\n");
+        
+        PROCESS_YIELD_UNTIL(etimer_expired(&timer) || etimer_expired(&registration_timer));
+       	
+        if (etimer_expired(&registration_timer)){
+            // SERVER REGISTRATION 
+            if(!registered){
+                //server_registration();
+                char *service_url = "/registration";
+                // Prepare the message
+                coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+                coap_set_header_uri_path(request, service_url);
+                // Set the payload 
+                const char msg[] = "{\"Type\":\"sensor\", \"Resource\":\"oxygen\"}";
+                coap_set_payload(request, (uint8_t *)msg, sizeof(msg) - 1);
+                // Issue the request in a blocking manner
+                // The client will wait for the server to reply (or the transmission to timeout)
+                printf("before blocking...\n");
+                COAP_BLOCKING_REQUEST(&server_ep, request, wait_for_ack);
+                printf("after blocking...\n");
+                // if the registration was not successful, we have to do it again
+                if(!registered){
+                    etimer_reset(&registration_timer);
+                }
+                
+                // END SERVER REGISTRATION
+            }
+            
+        }
+        
+        else if (etimer_expired(&registration_timer)){
+                if(registered){
+                    printf("trigerring device...\n");
+                    oxygen_measuring_device.trigger(); //this will call the res_event_handler of the resource that will randomly change the oxygen level
+		            //reset timer
+		            etimer_reset(&timer);
+                    printf("device is triggered...\n");
+                }
+            }
+		
 		    
     }
 
